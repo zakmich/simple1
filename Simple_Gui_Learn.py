816c7aea79be84235482a77e46767d4fe66baa2c
zakmich <michal.zak93@gmail.com>
2022-09-24 16:26:54 +0200

    Podstawowa konfiguracja SimpleGUI

--- Simple_Gui_Learn.py
+++ Simple_Gui_Learn.py
@@ -1,1 +1,35 @@
-Hello Projekt
+import PySimpleGUI as sg
+
+layout = [
+    [sg.Text('Text'),sg.Spin(['item1', 'item2'])],
+    [sg.Button('Button')],
+    [sg.Input()],
+    [sg.Text('Bogato'),sg.Button('Koniec')],
+
+]
+
+window = sg.Window("Converter",layout)
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
