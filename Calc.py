47b24337cdfc1ec7cf62ce0cdb67950741d258b8
zakmich <michal.zak93@gmail.com>
2022-09-26 22:21:41 +0200

    Working Calculator

--- Calc.py
+++ Calc.py
@@ -1,24 +1,51 @@
 import PySimpleGUI as sg
 
-layout = [
-    [sg.Text('Output', key = "-OUTPUT-")],
-    [sg.Input(key = '-INPUT-')],
-    [sg.Button('clear', key = '-BUTTON_CLEAR-'), sg.Button('enter', key = '-BUTTON_ENTER-')],
-    [sg.Button('7', key = '-BUTTON7-'), sg.Button('8', key = '-BUTTON8-'), sg.Button('9', key = '-BUTTON9-'), sg.Button('*', key = '-BUTTON*-')],
-    [sg.Button('4', key = '-BUTTON4-'), sg.Button('5', key = '-BUTTON5-'), sg.Button('6', key = '-BUTTON6-'), sg.Button('/', key = '-BUTTON/-')],
-    [sg.Button('1', key = '-BUTTON1-'), sg.Button('2', key = '-BUTTON2-'), sg.Button('3', key = '-BUTTON3-'), sg.Button('+', key = '-BUTTON+-')],
-    [sg.Button('0', key = '-BUTTON0-'), sg.Button(',', key = '-BUTTON.-'), sg.Button('-', key = '-BUTTON--')]
 
+sg.theme('Python')
+sg.set_options(font = 'Calibri 13', button_element_size = (6,3))
+button_size = (6,3)
+
+layout = [
+    [sg.Text('', key = '-TEXT-', font = 'Calibri 25', justification = 'right', expand_x = True, pad = (5,20), background_color = 'white')],
+    [sg.Button('clear', expand_x = True), sg.Button('enter', expand_x = True)],
+    [sg.Button('7', size = button_size), sg.Button('8', size = button_size), sg.Button('9', size = button_size), sg.Button('*', size = button_size)],
+    [sg.Button('4', size = button_size), sg.Button('5', size = button_size), sg.Button('6', size = button_size), sg.Button('/', size = button_size)],
+    [sg.Button('1', size = button_size), sg.Button('2',size = button_size), sg.Button('3', size = button_size), sg.Button('+', size = button_size)],
+    [sg.Button('0', expand_x = True), sg.Button('.', size = button_size), sg.Button('-', size = button_size)]
 
 ]
-
 window = sg.Window("Calculator",layout)
+
+current_number = []
+operation = []
 
 while True:
     event, values = window.read()
 
     if event == sg.WIN_CLOSED:
         break
+
+    if event in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']:
+        current_number.append(event)
+        join_number = ''.join(current_number)
+        window['-TEXT-'].update(join_number)
+
+    if event in ['+', '-', '/', '*' ]:
+        operation.append(''.join(current_number))
+        current_number = []
+        operation.append(event)
+        window['-TEXT-'].update('')
+
+    if event == 'enter':
+        operation.append(''.join(current_number))
+        result = eval(''.join(operation))
+        window['-TEXT-'].update(result)
+        operation = []
+
+    if event == 'clear':
+        current_number = []
+        operation = []
+        window['-TEXT-'].update('')
 
 
 window.close()
