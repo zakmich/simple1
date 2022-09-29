ec6420820c6249cae5a41fc302e9def1376847e3
zakmich <michal.zak93@gmail.com>
2022-09-29 22:13:35 +0200

    Final version

--- Stopwatch.py
+++ Stopwatch.py
@@ -1,22 +1,56 @@
 import PySimpleGUI as sg
+from time import time
 
-sg.theme('black')
-layout = [
-    [sg.Push(), sg.Image('xmark.png', pad = 0, enable_events = True, key = '-CLOSE-')],
-    [sg.VPush()],
-    [sg.Text('Time', font = 'Calibri 25')],
-    [
-        sg.Button('Start', button_color = ('#FFFFFF', '#FF0000'), border_width = 0),
-        sg.Button('Lap', button_color = ('#FFFFFF', '#FF0000'), border_width = 0)],
-    [sg.VPush()]
-]
+def create_window():
+    sg.theme('black')
+    layout = [
+        [sg.Push(), sg.Image('xmark.png', pad = 0, enable_events = True, key = '-CLOSE-')],
+        [sg.VPush()],
+        [sg.Text('0.0', font = 'Calibri 50', key = '-TIME-')],
+        [
+            sg.Button('Start', button_color = ('#FFFFFF', '#FF0000'), border_width = 0, key = '-STARTSTOP-'),
+            sg.Button('Lap', button_color = ('#FFFFFF', '#FF0000'), border_width = 0, key = '-LAP-', visible = False)],
+        [sg.Column([[]], key = '-LAPS-')],
+        [sg.VPush()]
+    ]
 
-window = sg.Window('Stopwatch',layout, size =(300,300), no_titlebar = True, element_justification = 'center')
+    return sg.Window('Stopwatch',layout, size =(300,300), no_titlebar = True, element_justification = 'center')
+
+window = create_window()
+start_time = 0
+active = False
+lap_amount = 1
 
 while True:
-    event, values = window.read()
-
+    event, values = window.read(timeout = 10)
     if event in (sg.WIN_CLOSED, '-CLOSE-'):
         break
+
+    if event == '-STARTSTOP-':
+        if active:
+            active = False
+            window['-STARTSTOP-'].update('Reset')
+            window['-LAP-'].update(visible = False)
+        else:
+            if start_time > 0:
+                window.close()
+                window = create_window()
+                start_time = 0
+                lap_amount = 1
+            else:
+                start_time = time()
+                active = True
+                window['-STARTSTOP-'].update('Stop')
+                window['-LAP-'].update(visible = True)
+
+    if active:
+        current_time = round(time() - start_time, 1)
+        window['-TIME-'].update(current_time)
+
+    if event == '-LAP-':
+        window.extend_layout(window['-LAPS-'], [[sg.Text(lap_amount),sg.VSeparator(),sg.Text(current_time)]])
+        lap_amount += 1
+
+
 
 window.close()
