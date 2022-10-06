efea5701ef7424f433f6e61253585c62c2854204
zakmich <michal.zak93@gmail.com>
2022-10-06 21:50:46 +0200

    Counter and Visual upgrades added

--- Guess.py
+++ Guess.py
@@ -1,10 +1,12 @@
 import PySimpleGUI as sg
 from random import randint
 
+sg.theme('DarkBlue')
+
 layout = [
-    [sg.Text('Guess number from range 1-100')],
-    [sg.Input(key = '-INPUT-'), sg.Button('CHECK', key = '-CHECK-')],
-    [sg.Text('Output', key = '-OUTPUT-')]
+    [sg.Text('Guess number from range 1-100', font = 'calibri 20', justification = 'center', expand_x = True)],
+    [sg.Input(font = 'calibri 15', key = '-INPUT-'), sg.Button('CHECK', font = 'calibri', border_width = 0,  key = '-CHECK-')],
+    [sg.Text('Result', font = 'calibri 15', key = '-OUTPUT-'), sg.Text('Attempt', font = 'calibri 15', key = '-TRY-', justification = 'right', expand_x = True)]
 ]
 
 window = sg.Window("Guess",layout)
@@ -12,6 +14,7 @@ window = sg.Window("Guess",layout)
 x = randint(1,100)
 number = 0
 print(x)
+counter = 0
 
 while True:
     event, values = window.read()
@@ -22,12 +25,16 @@ while True:
     if event == '-CHECK-':
         input_value = int(values['-INPUT-'])
         if x > input_value:
-            output_string = 'Too low'
+            output_string = 'Too low.'
+            counter += 1
         if x < input_value:
-            output_string = 'Too high'
+            output_string = 'Too high.'
+            counter += 1
         if x == input_value:
-            output_string = 'Score!'
+            output_string = 'You got it!'
+            counter += 1
 
-    window['-OUTPUT-'].update(output_string)
+        window['-OUTPUT-'].update(output_string)
+        window['-TRY-'].update(f'Number of attempts: {counter}')
 
 window.close()
