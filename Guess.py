ecff2da100e376eb4a3df79294fb5bc963892ae2
zakmich <michal.zak93@gmail.com>
2022-10-07 21:44:32 +0200

    Visual upgrade, input field cleansing after every try, restart button added

--- Guess.py
+++ Guess.py
@@ -1,18 +1,20 @@
 import PySimpleGUI as sg
 from random import randint
 
-sg.theme('DarkBlue')
+def new_window():
+    sg.theme('DarkBlue')
+    layout = [
+        [sg.VPush(),sg.Text('Guess number from range 1-100', font = 'calibri 14', justification = 'center', expand_x = True)],
+        [sg.Button('RESET', font = 'calibri', border_width = 0,  key = '-RESET-'),sg.Input(font = 'calibri 15', key = '-INPUT-', size = (5,10), justification = 'center', expand_x = True),
+         sg.Button('CHECK', font = 'calibri', border_width = 0,  key = '-CHECK-')],
+        [sg.Text('Result', font = 'calibri 12', key = '-OUTPUT-'), sg.Text('Attempt', font = 'calibri 12', key = '-TRY-', justification = 'right', expand_x = True)],
+        [sg.VPush()]
+    ]
 
-layout = [
-    [sg.Text('Guess number from range 1-100', font = 'calibri 20', justification = 'center', expand_x = True)],
-    [sg.Input(font = 'calibri 15', key = '-INPUT-'), sg.Button('CHECK', font = 'calibri', border_width = 0,  key = '-CHECK-')],
-    [sg.Text('Result', font = 'calibri 15', key = '-OUTPUT-'), sg.Text('Attempt', font = 'calibri 15', key = '-TRY-', justification = 'right', expand_x = True)]
-]
+    return sg.Window("Guess",layout, size = (300,150))
 
-window = sg.Window("Guess",layout)
-
+window = new_window()
 x = randint(1,100)
-number = 0
 print(x)
 counter = 0
 
@@ -21,8 +23,10 @@ while True:
 
     if event == sg.WIN_CLOSED:
         break
-
+    if event == '-RESET-':
+        window = new_window()
     if event == '-CHECK-':
+        window['-INPUT-'].update('')
         input_value = int(values['-INPUT-'])
         if x > input_value:
             output_string = 'Too low.'
